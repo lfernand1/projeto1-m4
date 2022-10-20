9383256007714e32fdc643c8dedbcccbe925baf9
Larissa Fernanda <larifernand7@gmail.com>
2022-10-20 05:02:48 -0300

    foto

---
+++ cardapio-el-geladon/src/PaletaLista.jsx
@@ -0,0 +1,37 @@
+import "PaletaLista.css";
+import "PaletaLista.css"
+import { paletas } from "mocks/paletas.js";
+
+function PaletaLista() {
+  return (
+<div className="PaletaLista">
+    {paletas.map((paleta, index) => (
+    <div className="PaletaListaItem">
+        <div>
+        <div className="PaletaListaItem__titulo"> {paleta.titulo} </div>
+        <div className="PaletaListaItem__preco">
+            {" "}
+            R$ {paleta.preco.toFixed(2)}{" "}
+        </div>
+        <div className="PaletaListaItem__descricao">
+            {" "}
+            {paleta.descricao}{" "}
+        </div>
+        <div className="PaletaListaItem__acoes Acoes">
+            <button className="Acoes__adicionar Acoes__adicionar--preencher">
+            adicionar
+            </button>
+        </div>
+        </div>
+        <img
+        className="PaletaListaItem__foto"
+        src={paleta.foto}
+        alt={`Paleta de ${paleta.sabor}`}
+        />
+    </div>
+    ))}
+</div>
+  );
+}
+
+export default PaletaLista;
