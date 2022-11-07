a4988ec2d59232771baeb164e8c2e3d80ca94c9c
Larissa Fernanda <larifernand7@gmail.com>
2022-11-07 15:23:24 -0300

    aaaa

--- cardapio-el-geladon/src/PaletaLista.jsx
+++ cardapio-el-geladon/src/PaletaLista.jsx
@@ -1,36 +1,46 @@
+import { useState } from "react";
 import "PaletaLista.css";
-import "PaletaLista.css"
 import { paletas } from "mocks/paletas.js";
 
 function PaletaLista() {
+  const [paletaSelecionada, setPaletaSelecionada] = useState({});
+
+  const adicionarItem = (paletaIndex) => {
+    const paleta = {
+      [paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) + 1,
+    };
+    setPaletaSelecionada({ ...paletaSelecionada, ...paleta });
+    <button className="Acoes__adicionar Acoes__adicionar--preencher" onClick={() => adicionarItem(index)}>adicionar</button>
+  };
+
   return (
-<div className="PaletaLista">
-    {paletas.map((paleta, index) => (
-    <div className="PaletaListaItem" key={`PaletaListaItem-${index}`} >
-        <div>
-        <div className="PaletaListaItem__titulo"> {paleta.titulo} </div>
-        <div className="PaletaListaItem__preco">
-            {" "}
-            R$ {paleta.preco.toFixed(2)}{" "}
+    <div className="PaletaLista">
+      {paletas.map((paleta, index) => (
+        <div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
+          <div>
+            <div className="PaletaListaItem__titulo"> {paleta.titulo} </div>
+            <div className="PaletaListaItem__preco">
+              {" "}
+              R$ {paleta.preco.toFixed(2)}{" "}
+            </div>
+            <div className="PaletaListaItem__descricao">
+              {" "}
+              {paleta.descricao}{" "}
+            </div>
+            <div className="PaletaListaItem__acoes Acoes">
+              <button className="Acoes__adicionar Acoes__adicionar--preencher">
+                adicionar
+              </button>
+            </div>
+          </div>
+          <img
+            className="PaletaListaItem__foto"
+            src={paleta.foto}
+            alt={`Paleta de ${paleta.sabor}`}
+          />
         </div>
-        <div className="PaletaListaItem__descricao">
-            {" "}
-            {paleta.descricao}{" "}
-        </div>
-        <div className="PaletaListaItem__acoes Acoes">
-            <button className="Acoes__adicionar Acoes__adicionar--preencher">
-            adicionar
-            </button>
-        </div>
-        </div>
-        <img
-        className="PaletaListaItem__foto"
-        src={paleta.foto}
-        alt={`Paleta de ${paleta.sabor}`}
-        />
+      ))}
     </div>
-    ))}
-</div>
   );
 }
 
